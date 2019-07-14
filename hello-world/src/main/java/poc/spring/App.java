73be76aa1828135a47dc0613e358283129564449
Sanjay <Sanjay.X.Ghosh@gmail.com>
2019-07-14 15:29:39 +0530

    Adding the changes

--- hello-world/src/main/java/poc/spring/App.java
+++ hello-world/src/main/java/poc/spring/App.java
@@ -2,18 +2,32 @@ package poc.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
  *
  */
-public class App 
+@SpringBootApplication
+@EnableAutoConfiguration
+@RestController
+public class App
 {
 	private static Logger LOG = LoggerFactory.getLogger("App");
+	
+	@GetMapping("/helloWorld")
+	public String helloWorld() {
+		return "helloWorld";
+	}
 	
     public static void main( String[] args )
     {
     	LOG.info( "Hello World!" );
-        
+    	ApplicationContext context = SpringApplication.run(App.class, args);
     }
 }

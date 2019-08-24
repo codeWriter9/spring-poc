ea955661d09cec84835b2281cd73152b6fc097b1
Sanjay <Sanjay.X.Ghosh@gmail.com>
2019-08-24 21:24:21 +0530

    ADD - config for csv file reading

--- swagger-rest-client/src/main/java/com/ghosh/sanjay/App.java
+++ swagger-rest-client/src/main/java/com/ghosh/sanjay/App.java
@@ -1,5 +1,7 @@
 package com.ghosh.sanjay;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -16,9 +18,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class App implements CommandLineRunner {
 	
+	private static Logger LOG = LoggerFactory.getLogger(App.class.getName());
+	
 	@Override
 	public void run(String... args) throws Exception {		
-		
+		LOG.info("Server Started !!");
 	}
 
 	public static void main(String[] args) {
--- swagger-rest-client/src/main/java/com/ghosh/sanjay/config/AppConfig.java
+++ swagger-rest-client/src/main/java/com/ghosh/sanjay/config/AppConfig.java
@@ -3,23 +3,21 @@ package com.ghosh.sanjay.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-@ComponentScan("com.ghosh.sanjay")
+@Component
 public class AppConfig {
 
-	@Bean
-	public RestTemplate restTemplate() {		
-		return new RestTemplateBuilder(restTemplateCustomizer()).basicAuthentication("admin", "admin")
-				.build();
+	@Bean("restTemplate")
+	public RestTemplate restTemplate() {
+		return new RestTemplateBuilder(restTemplateCustomizer()).basicAuthentication("admin", "admin").build();
 	}
 
 	@Bean
 	public RestTemplateCustomizer restTemplateCustomizer() {
 		return (restTemplate) -> {
-			// TODO 
+			// TODO
 		};
 	}
-
 }

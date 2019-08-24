52d5e1b342744c55bf9a129d80a226b630c4ed8c
Sanjay <Sanjay.X.Ghosh@gmail.com>
2019-08-24 15:34:49 +0530

    ADD config for Spring boot run and swagger

--- swagger-rest-client/src/main/java/com/ghosh/sanjay/App.java
+++ swagger-rest-client/src/main/java/com/ghosh/sanjay/App.java
@@ -1,13 +1,28 @@
 package com.ghosh.sanjay;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableSwagger2
+public class App implements CommandLineRunner {
+	
+	@Override
+	public void run(String... args) throws Exception {		
+		
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
 }

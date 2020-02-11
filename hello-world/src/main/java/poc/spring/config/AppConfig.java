9c3b09d5e620f83991bd3fbfd43c841fbf3ccc55
Sanjay <Sanjay.X.Ghosh@gmail.com>
2020-02-11 22:57:36 +0530

    ADD - javadocs and log statement
    Javadocs to explain intent, log statement to confirm application run

--- hello-world/src/main/java/poc/spring/config/AppConfig.java
+++ hello-world/src/main/java/poc/spring/config/AppConfig.java
@@ -9,15 +9,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+/**
+ * 
+ * App Configuration which loads the configuration
+ * We are using <code>@Configuration</code> and 
+ * <code>@PropertySource</code> to load the configuration
+ * 
+ * @author Sanjay Ghosh
+ *
+ */
 @Configuration
 @PropertySource("classpath:data/helloWorld.properties")
 public class AppConfig {
 	
 	private static Logger LOG = getLogger(lookup().lookupClass());
 	
+	/**
+	 * 
+	 * Read the property from the property files
+	 * 
+	 */
 	@Value("${helloWorld}")
 	private String helloWorld;
 	
+	/**
+	 * 
+	 * Instead of hard coding we are setting the hello world as a bean in the context
+	 * 
+	 * @return String
+	 */
 	@Bean
 	public String helloWorld() {
 		return helloWorld;
--- hello-world/src/main/java/poc/spring/controller/HelloWorldController.java
+++ hello-world/src/main/java/poc/spring/controller/HelloWorldController.java
@@ -8,16 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 
+ * REST Controller 
+ * 
+ * @author Sanjay Ghosh
+ *
+ */
 @RestController
 public class HelloWorldController {
 
 	private static Logger LOG = getLogger(lookup().lookupClass());
 
+	/**
+	 * 
+	 * Using the String property as a bean
+	 * 
+	 */
 	@Autowired
 	private String helloWorld;
 
+	/**
+	 * 
+	 * Rest Controller
+	 * 
+	 * @return String
+	 */
 	@GetMapping("/helloWorld")
 	public String helloWorld() {
+		LOG.info("returning from GET method");
 		return helloWorld;
 	}
 }

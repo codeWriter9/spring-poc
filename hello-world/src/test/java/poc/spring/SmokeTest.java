70dea587218cd3053e2fbc512bb3a02312e13cd6
codeWriter9 <Sanjay.X.Ghosh@gmail.com>
2021-02-05 22:46:20 +0530

    Add - more tests for web testing

---
+++ hello-world/src/test/java/poc/spring/SmokeTest.java
@@ -0,0 +1,44 @@
+package poc.spring;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import poc.spring.controller.HelloWorldController;
+import poc.spring.service.HelloWorldService;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class SmokeTest {
+	
+	@Autowired
+	private HelloWorldController controller;
+
+	@Autowired
+	private HelloWorldService service;
+	
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+		assertThat(service).isNotNull();
+		assertThat(port).isNotNull();
+		assertThat(restTemplate).isNotNull();
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/helloWorld",
+				String.class)).contains("helloWorld!");
+	}
+
+}
---
+++ hello-world/src/test/java/poc/spring/WebMockTest.java
@@ -0,0 +1,44 @@
+package poc.spring;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import poc.spring.config.AppConfig;
+import poc.spring.controller.HelloWorldController;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloWorldController.class)
+@ContextConfiguration(classes = { AppConfig.class, App.class })
+public class WebMockTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private HelloWorldController controller;
+	
+	@Test
+	public void checkNotNull() throws Exception {
+		assertThat(controller).isNotNull();		
+		assertThat(mockMvc).isNotNull();		
+	}
+	
+
+	@Test
+	public void greetingShouldReturnMessageFromService() throws Exception {		
+		this.mockMvc.perform(get("/helloWorld")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("helloWorld!")));
+	}
+}

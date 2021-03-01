68d499e0410fd2ec99611e522d7e11a7b4eecdf4
Sanjay <SanjayXGhosh@gmail.com>
2021-03-01 05:37:48 +0000

    Fix - Spring Boot config and logback binding

--- xml-json-converter/src/main/java/com/ghosh/sanjay/App.java
+++ xml-json-converter/src/main/java/com/ghosh/sanjay/App.java
@@ -1,14 +1,32 @@
 package com.ghosh.sanjay;
 
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
-
-public class App
+@SpringBootApplication
+public class App implements CommandLineRunner
 {
+
+    private static Logger LOG = getLogger(lookup().lookupClass());
+
+    @Override
+    public void run(String... args) throws Exception {
+	LOG.info("Hello World");
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+	SpringApplication.run(App.class, args);
     }
 }
--- xml-json-converter/src/test/java/com/ghosh/sanjay/beans/FileUtilTest.java
+++ xml-json-converter/src/test/java/com/ghosh/sanjay/beans/FileUtilTest.java
@@ -9,16 +9,28 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import lombok.extern.slf4j.Slf4j;
 
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
 import static java.lang.String.*;
 import static java.nio.file.Files.*;
 import static java.nio.file.Paths.*;
 
 
-@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
 public class FileUtilTest {
+
+	private static Logger LOG = getLogger(lookup().lookupClass());
 
 	private String employeeXml;
 
@@ -30,7 +42,7 @@ public class FileUtilTest {
 	@Test
 	public void testIfFileLoaded() {
 		Assert.assertNotNull(employeeXml);
-		log.info(employeeXml);
+		LOG.info(employeeXml);
 	}
 
 	@After

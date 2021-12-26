0ed06cca49d36fb7baf34d311b600974d8153ca8
Sanjay <SanjayXGhosh@gmail.com>
2021-12-26 16:45:04 +0000

    Add - CRUD bash REST curl

--- spring-jpa-mongo/curl_commands.bash
+++ spring-jpa-mongo/curl_commands.bash
@@ -1,4 +1,6 @@
 curl -i -X POST -H "Content-Type:application/json" -d '{ "name" : "Jindal Takla",  "location" : "Jhumritelaiya",  "username" : "TaklaJ" }' http://localhost:8080/user
 curl -i -X GET -H "Content-Type:application/json" http://localhost:8080/user
-curl -i -X PUT -H "Content-Type:application/json" -d '{ "name" : "Peter Parker",  "location" : "New York",   "username" : "parker" }' http://localhost:8080/user/61c890ef127f95221632db20
+curl -i -X PUT -H "Content-Type:application/json" -d '{ "name" : "Peter Parker",  "location" : "New York",   "username" : "parker" }' http://localhost:8080/user/61c89b33439fad212794c62b
+curl -i -X GET -H "Content-Type:application/json" http://localhost:8080/user
+curl -i -X DELETE -H "Content-Type:application/json" http://localhost:8080/user/61c89b33439fad212794c62b
 curl -i -X GET -H "Content-Type:application/json" http://localhost:8080/user
